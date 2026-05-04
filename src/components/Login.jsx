c94a91a7179ed178dc55decae26562f09125b358
saurav pawar <pawarsaurav957@gmail.com>
2026-05-04 12:46:16 +0530

    setup the redux store and created an userSlice

---
+++ src/components/Login.jsx
@@ -0,0 +1,77 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constant";
+
+const Login = () => {
+  const [emailId, setEmailId] = useState("patil@gmail.com");
+  const [password, setPassword] = useState("Patil@123");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/login",
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true }, // it will sent the token back
+      );
+      dispatch(addUser(res.data));
+      return navigate("/");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="flex justify-center my-10">
+      <div className="card card-dash bg-base-200 w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title justify-center text-2xl font-semibold text-white">
+            Login
+          </h2>
+
+          <div className="px-3 my-4">
+            <label className="label py-1">
+              <span className="label-text text-white">Email Id</span>
+            </label>
+            <input
+              type="text"
+              className="input input-bordered w-full"
+              value={emailId}
+              onChange={(e) => setEmailId(e.target.value)}
+            />
+          </div>
+
+          <div className="px-3 my-4">
+            <label className="label py-1">
+              <span className="label-text text-white">Password</span>
+            </label>
+            <input
+              type="text"
+              className="input input-bordered w-full"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <div className="card-actions justify-center py-4">
+            <button
+              className="btn w-24 bg-purple-600 hover:bg-purple-700 text-white border-none"
+              onClick={handleLogin}
+            >
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
